97acf7fb011bc9d0cb2fd0df7654d6cd70d3422d
daryl <daryl.mcafee@gmail.com>
2023-06-02 17:00:50 -0400

    Added swapping between HTML files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let playButton = document.querySelector("#play-button")
+//console.log(playButton)
+
+playButton.addEventListener("click", function(){
+    //console.log("button clicked")
+    //swapping between html files
+    window.location.assign("index2.html")
+})
