baa3e9e1ebc524ff957e77a11008aa650ac76738
daryl <daryl.mcafee@gmail.com>
2023-06-03 13:00:52 -0400

    Simplified JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
-let playButton = document.querySelector("#play-button")
-//console.log(playButton)
-
-playButton.addEventListener("click", function(){
-    //console.log("button clicked")
-    //swapping between html files
+function startGame() {
     window.location.assign("index2.html")
-})
+}
