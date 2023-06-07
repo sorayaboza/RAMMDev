b2a235cba559d8eb0b2c6aaac1264d019610b4c4
Soraya B <sorayaboza@gmail.com>
2023-06-07 16:07:19 -0700

    added temp file

--- js/temp.js
+++ js/temp.js
@@ -1,12 +1,3 @@
-function startGame() {
-    window.location.assign("game.html")
-}
-
-function startMenu() {
-    window.location.assign("index.html")
-}
-
-// ----- TEXT BOXES BELOW ----- credit: https://www.youtube.com/watch?v=8i2K7uwh124
 
 var container = document.querySelector(".text")
 var currentLineIndex = 0; // Keep track of the current line index
@@ -22,7 +13,7 @@ var speeds = {
 var textLines = [
     { string: "...", speed: speeds.normal},
     { string: "Hello...", speed: speeds.normal},
-    { string: "... djmejay-san", speed: speeds.slow, classes:["red"]},
+    { string: "...djmejay-san", speed: speeds.slow, classes:["red"]},
 ]
 
 // Creating a <span> for each of the characters in the textLines.string
@@ -47,27 +38,46 @@ textLines.forEach((line, index) => {
         })
     })
 })
-
 // This function will call itself until the process is done
 function revealOneCharacter(list) {
     // Gives us back a list of everything we removed from the array, and we only want the first one.
     var next = list.splice(0, 1)[0];
-    next.span.classList.add("revealed")
+    next.span.classList.add("revealed");
     next.classes.forEach((c) => {
-        next.span.classList.add(c)
-    })
-
-    var delay = next.isSpace ? 0: next.delayAfter // No delay on spaces.
-    
-    // Everytime the function runs, the list gets 1 smaller. Once it reaches 0, we stop calling the function recursively
+      next.span.classList.add(c);
+    });
+  
+    var delay = next.isSpace ? 0 : next.delayAfter; // No delay on spaces.
+  
+    // Every time the function runs, the list gets 1 smaller. Once it reaches 0, we stop calling the function recursively
     if (list.length > 0) {
-        setTimeout(function() {
-            revealOneCharacter(list)
-        }, delay)
+      setTimeout(function () {
+        revealOneCharacter(list);
+      }, delay);
+    } else {
+      // Delay before clearing the text box
+      setTimeout(function () {
+        container.textContent = ""; // Clear the text box
+        // Trigger the next line after clearing the text box
+        if (textLines[currentLineIndex + 1]) {
+          currentLineIndex++;
+          characters = [];
+          textLines[currentLineIndex].string.split("").forEach((character) => {
+            var span = document.createElement("span");
+            span.textContent = character;
+            container.appendChild(span);
+            characters.push({
+              span: span,
+              isSpace: character === " ",
+              delayAfter: textLines[currentLineIndex].speed,
+              classes: textLines[currentLineIndex].classes || [],
+            });
+          });
+          revealOneCharacter(characters);
+        }
+      }, 1000); // Adjust the delay (in milliseconds) before clearing the text box
     }
-}
-
-// Reveals each character in the list
-revealOneCharacter(characters)
-
-// ----- TEXT BOXES ABOVE -----
+  }
+  
+  // Reveals each character in the list
+  revealOneCharacter(characters);
